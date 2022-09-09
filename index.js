59d94e7bcff4d62331c542fbd37ee081e8f0f7c9
Elano N. Caitano <nunes.elano14@gmail.com>
2022-09-08 22:15:15 -0300

    Timestamp Microservice works

--- index.js
+++ index.js
@@ -10,23 +10,56 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
 
+// --------------------------------------------------------------------------
+
+// Minha solução
+
+app.get("/api", (req, res) => {
+  const now = new Date();
+  res.json({ unix: now.getTime(), utc: now.toUTCString() })
+});
+
+app.get("/api/:date", function (req, res) {
+
+  let dateString = req.params.date;
+  let invalidDate = "Invalid Date";
+  let date;
+
+  if (parseInt(dateString) < 10000) {   // 5 ou mais digitos devem formar uma hora unix
+    date = new Date(dateString);
+  } else {
+    console.log(parseInt(dateString));
+    date = new Date(parseInt(dateString));
+  }
+
+  if (date.toString() === invalidDate) {
+    res.json({ error: invalidDate});
+  } else {
+    res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+  }
+
+});
+
+// --------------------------------------------------------------------------
+
 
 // listen for requests :)
+
+
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+// app.listen(5432, () => console.log("Server is running!"));
