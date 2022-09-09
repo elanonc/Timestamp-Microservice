8d263919d63a1e6fce221a43c53ff27cdad508c7
Elano N. Caitano <nunes.elano14@gmail.com>
2022-09-08 23:08:03 -0300

    Corrigindo erro no github pages

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const url = require("url");
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + 'index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get("/api/hello", function (req, res) {
