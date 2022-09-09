6b9faf35105967ed8bbf6e019e73a8832a14d514
Elano N. Caitano <nunes.elano14@gmail.com>
2022-09-08 23:15:47 -0300

    Corrigindo erros

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-const url = require("url");
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 app.use(express.static('public'));
